a98e46ee1be95d3c269eae930287151eb2d62b01
daraermak41@gmail.com <daraermak41@gmail.com>
2025-03-11 19:27:58 +0300

    Added source files

---
+++ src/test/java/com/yarmak/event_planner/EventPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yarmak.event_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
